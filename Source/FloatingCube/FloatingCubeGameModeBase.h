533e592065a1721bcd903d28b8342b1d3fd5581d
Geogeez <Geogeez@users.noreply.github.com>
2023-04-12 17:28:17 -0400

    Initial Commit

---
+++ Source/FloatingCube/FloatingCubeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FloatingCubeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLOATINGCUBE_API AFloatingCubeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
